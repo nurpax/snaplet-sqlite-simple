f874eb75956158a8bc816a0dfaac987199824172
Janne Hellsten <jjhellst@gmail.com>
2012-11-27 00:34:05 +0200

    Don't start a server for testing snaplet-sqlite-simple handlers
    No need to start a web server just to test simple db init / query
    functions.  So delete server init & start up code.

--- test/src/TestSuite.hs
+++ test/src/TestSuite.hs
@@ -4,12 +4,9 @@
 module Main where
 
 ------------------------------------------------------------------------------
-import           Control.Concurrent
 import           Control.Exception hiding (Handler)
 import           Control.Monad
 import           Prelude hiding (catch)
-import           Snap.Http.Server.Config
-import           System.IO
 import           Test.Framework
 ------------------------------------------------------------------------------
 
@@ -26,35 +23,7 @@ main =
   inDir True "non-cabal-appdir" runTests
   where
     runTests = do
-      (tid, mvar) <- startServer
-      defaultMain [tests] `finally` killThread tid
-      putStrLn "waiting for termination mvar"
-      takeMVar mvar
+      (_, _handler, _doCleanup) <- runSnaplet Nothing appInit
+      defaultMain [tests] `finally` (return ())
 
-    tests = mutuallyExclusive $
-              testGroup "snaplet-sqlite-simple" [Tests.tests]
-
-
-------------------------------------------------------------------------------
-startServer :: IO (ThreadId, MVar ())
-startServer = do
-  mvar <- newEmptyMVar
-  t    <- forkIO $ serve mvar (setPort 9753 defaultConfig) appInit
-  threadDelay $ 2*10^(6::Int)
-  return (t, mvar)
-  where
-    serve mvar config initializer =
-      flip finally (putMVar mvar ()) $
-      handle handleErr $ do
-        hPutStrLn stderr "initializing snaplet"
-        (_, handler, doCleanup) <- runSnaplet Nothing initializer
-
-        flip finally doCleanup $ do
-          (conf, site) <- combineConfig config handler
-          hPutStrLn stderr "bringing up server"
-          simpleHttpServe conf site
-          hPutStrLn stderr "server killed"
-
-    handleErr :: SomeException -> IO ()
-    handleErr e = hPutStrLn stderr $ "startServer exception: " ++ show e
-
+    tests = mutuallyExclusive $ testGroup "snaplet-sqlite-simple" [Tests.tests]
