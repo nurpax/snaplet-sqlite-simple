cdc087d5624e177ab5db7330ee6588a65cb694b6
Janne Hellsten <jjhellst@gmail.com>
2012-11-28 00:01:59 +0200

    Add simple unit tests for creating and updating users

--- test/src/Tests.hs
+++ test/src/Tests.hs
@@ -20,9 +20,10 @@ import           Snap.Snaplet.Test
 
 ------------------------------------------------------------------------------
 tests :: Test
-tests = testGroup "Snap.Snaplet.SqliteSimple"
-    [mutuallyExclusive $
-       testGroup "createUser tests" [testCreateUserGood]]
+tests = mutuallyExclusive $ testGroup "Snap.Snaplet.SqliteSimple"
+    [ testGroup "createUser tests" [testCreateUserGood]
+    , testGroup "update user tests" [testUpdateUser]
+    ]
 
 isRight :: Either a b -> Bool
 isRight (Left _) = False
@@ -36,6 +37,50 @@ testCreateUserGood = testCase "createUser good params" assertGoodUser
     assertGoodUser = do
       let hdl = with auth $ createUser "foo" "foo"
       res <- evalHandler (ST.get "" M.empty) hdl appInit
-      either (assertFailure . show) (assertBool failMsg . isRight) res
+      either (assertFailure . show) checkUserFields res
 
-    failMsg = "createUser failed: Couldn't create a new user."
+    checkUserFields (Left _) =
+      assertBool "createUser failed: Couldn't create a new user." False
+
+    checkUserFields (Right u) = do
+      assertEqual "login match"  "foo" (userLogin u)
+      assertEqual "login count"  0 (userLoginCount u)
+      assertEqual "fail count"   0 (userFailedLoginCount u)
+      assertEqual "local host ip" Nothing (userCurrentLoginIp u)
+      assertEqual "local host ip" Nothing (userLastLoginIp u)
+      assertEqual "locked until" Nothing (userLockedOutUntil u)
+      assertEqual "empty email" Nothing (userEmail u)
+
+------------------------------------------------------------------------------
+-- Create a user, modify it, persist it and load again, check fields ok.
+-- Must run after testCreateUserGood
+
+testUpdateUser :: Test
+testUpdateUser = testCase "createUser + update good params" assertGoodUser
+  where
+    assertGoodUser :: Assertion
+    assertGoodUser = do
+      let loginHdl = with auth $ loginByUsername "foo" (ClearText "foo") True
+      res <- evalHandler (ST.get "" M.empty) loginHdl appInit
+      either (assertFailure . show) checkLoggedInUser res
+
+    checkLoggedInUser (Left _) = assertBool "failed login" False
+    checkLoggedInUser (Right u) = do
+      assertEqual "login count"  1 (userLoginCount u)
+      assertEqual "fail count"   0 (userFailedLoginCount u)
+      assertEqual "locked until" Nothing (userLockedOutUntil u)
+      assertEqual "local host ip" (Just "127.0.0.1") (userCurrentLoginIp u)
+      assertEqual "no previous login" Nothing (userLastLoginIp u)
+      let saveHdl = with auth $ saveUser (u { userLogin = "bar" })
+      res <- evalHandler (ST.get "" M.empty) saveHdl appInit
+      either (assertFailure . show) checkUpdatedUser res
+
+    checkUpdatedUser (Left _) = assertBool "failed saveUser" False
+    checkUpdatedUser (Right u) = do
+      assertEqual "login rename ok?"  "bar" (userLogin u)
+      assertEqual "login count"  1 (userLoginCount u)
+      assertEqual "local host ip" (Just "127.0.0.1") (userCurrentLoginIp u)
+      assertEqual "local host ip" Nothing (userLastLoginIp u)
+      let loginHdl = with auth $ loginByUsername "bar" (ClearText "foo") True
+      res <- evalHandler (ST.get "" M.empty) loginHdl appInit
+      either (assertFailure . show) (assertBool "login as 'bar' ok?" . isRight) res
